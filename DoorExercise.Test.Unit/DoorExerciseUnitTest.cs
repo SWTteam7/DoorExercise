20117b51a6609d7c5433f0b230717910c3de7f62
Christiane95 <christiane@noerkjaer-svendsen.dk>
2018-03-01 11:14:48 +0100

    Add project files.

---
+++ DoorExercise.Test.Unit/DoorExerciseUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DoorExercise.Test.Unit
+{
+   [TestFixture]
+    public class DoorExerciseUnitTest
+   {
+      private IDoor door;
+      private IUserValidation uv;
+      private IAlarm alarm;
+      private IEntryNotification en;
+      private DoorControl uut;
+
+      [SetUp]
+      public void SetUp()
+      {
+         door = Substitute.For<IDoor>();
+         uv = Substitute.For<IUserValidation>();
+         alarm = Substitute.For<IAlarm>();
+         en = Substitute.For<IEntryNotification>();
+
+         uut = new DoorControl(door,uv,en,alarm);
+      }
+
+      [Test]
+      public void Const_state_closed()
+      {
+        
+      }
+    }
+}
---
+++ DoorExercise/DoorControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorExercise
+{
+    public class DoorControl
+    {
+       private IDoor _door;
+       private IUserValidation _uv;
+       private IEntryNotification _en;
+       private IAlarm _alarm;
+       private Doorstate state;
+
+       private enum Doorstate
+       {
+          Closed,
+          Opening,
+          Closing
+       }
+       public DoorControl(IDoor door, IUserValidation uv, IEntryNotification en, IAlarm alarm)
+       {
+          _door = door;
+          _uv = uv;
+          _en = en;
+          _alarm = alarm;
+
+          state = Doorstate.Closed;
+       }
+
+
+       public void RequestEntry(int id)
+       {
+          if (_uv.ValidateEntryRequest(id) == true)
+          {
+             _door.Open();
+             _en.NotifyEntryGranted();
+             state = Doorstate.Opening;
+          }
+       }
+
+       public void DoorOpened()
+       {
+          if (_door.Open() == true)
+          {
+             _door.Close();
+             state = Doorstate.Closing;
+          }
+       }
+
+       public void DoorClosed()
+       {
+          if (_door.Close() == true)
+          {
+             state = Doorstate.Closed;
+          }
+       }
+       
+    }
+}
